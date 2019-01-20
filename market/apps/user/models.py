2d0079f147fa1cdc5d8d85f3f7c75325ffd09ba9
rammusli <1299307207@qq.com>
2019-01-20 14:04:23 +0800

    提交项目日志

--- market/apps/user/models.py
+++ market/apps/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    sex_choices=((1,"男"),(2,"女"),(3,"保密"),)
+    name = models.CharField(max_length=50)
+    age = models.SmallIntegerField()
+    Gender = models.SmallIntegerField(choices=sex_choices,default=3)
+    is_delete = models.BooleanField(default= False) #假删除
