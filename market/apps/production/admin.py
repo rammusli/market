9ded9dbc2e3d3aa4a91695ef6e6718d39ae6556c
rammusli <1299307207@qq.com>
2019-01-24 19:31:37 +0800

    555

--- market/apps/production/admin.py
+++ market/apps/production/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 #Register your models here.
 from .models import ProCategory, ProductUnit, ProSPU,  ProSKU, PhotoAlbum,  Banner, Activity, ActivityRegion
 
-admin.site.register(ProCategory)
+class ArtcleAdimin(admin.ModelAdmin):
+    list_display = ('id','category_name','category_introduce','category_order','create_time','update_time')
+    ordering = ('-id',)
+admin.site.register(ProCategory,ArtcleAdimin)
 admin.site.register(ProductUnit)
 admin.site.register(ProSPU)
 admin.site.register(ProSKU)
